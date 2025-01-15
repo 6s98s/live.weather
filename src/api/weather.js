a113a8a1583c9e8bebd6d5e361314f24185d7164
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-15 15:16:48 +0900

    API 키와 URL 명시

---
+++ src/api/weather.js
@@ -0,0 +1,4 @@
+const getweather = async (lat, lon) => {
+    const API_KEY = 'de98638262183af60f758cd4a73fe4d6';
+    const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid={API_KEY}';
+} 
