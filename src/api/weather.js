4ccc4ed411b58e4a67bb34562f1ad08486b9c602
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-16 21:26:30 +0900

    Update weather.js

--- src/api/weather.js
+++ src/api/weather.js
@@ -1,3 +1,13 @@
-const function getWeather(city) {
-    const API_KEY = ``;
+import axios from 'axios';
+
+const API_KEY = `de98638262183af60f758cd4a73fe4d6`;
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=35.8714&lon=128.6014&appid=${API_KEY}`;
+
+export const getWeather = async () => {
+    try {
+        const { data } = await axios.get(API_URL);
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
 }
