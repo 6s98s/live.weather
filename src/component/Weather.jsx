ec7fe2356a02b33f6e8d040c08825ff30d06bf95
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-30 10:46:45 +0900

    일단 개발 중지 ...

--- src/component/Weather.jsx
+++ src/component/Weather.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import Weather from '../component/Weather';
+import WeatherDisplay from './WeatherDisplay';
 
 const App = () => {
   return (
     <div>
       <h3>대구의 날씨 님버스와 함께</h3>
       <Weather />
+      <WeatherDis />
     </div>
   );
 };
 
-export default App;
+export default Weather;
